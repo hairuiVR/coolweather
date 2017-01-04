603ee135e22965a0d2dc4e33c219dba18cc6b70f
hari <3037778949@qq.com>
2017-01-04 10:38:24 +0800

    Create SqliteDB model

---
+++ app/src/androidTest/java/model/Province.java
@@ -0,0 +1,35 @@
+package model;
+
+/**
+ * Created by chiqifeng on 2017/1/4.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
